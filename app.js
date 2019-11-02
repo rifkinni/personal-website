363b76f8a19c9ddb1338c3101d1e2044b7c90946
Nicole Rifkin <nicole@Nicoles-MacBook-Air.local>
2019-11-02 10:45:33 -0400

    favicon

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
