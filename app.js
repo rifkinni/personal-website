0ca558d6f9b71d2b7f3e04e09eef1f47fb119b69
Nicole Rifkin <nicole@Nicoles-MacBook-Air.local>
2020-11-29 09:58:31 -0500

    remove users router

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ var logger = require('morgan');
 var favicon = require('serve-favicon');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +20,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')))
 
 // catch 404 and forward to error handler
