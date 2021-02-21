c086b518e9327c016fee9585e2fc0331929c6537
Nicole Rifkin <nicole@Nicoles-MacBook-Air.local>
2021-02-21 18:26:07 -0500

    create portfolio page

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.get('/about', function(req, res, next) {
   res.render('about', { bio: bio() });
 });
 
+router.get('/portfolio', function(req, res, next) {
+  res.render('portfolio', { bio: bio(), portfolio: portfolio() });
+});
+
 router.get('/resume', function(req, res, next) {
   params = {
     bio: bio(),
@@ -22,10 +26,6 @@ router.get('/resume', function(req, res, next) {
 
 router.get('/cats', function(req, res, next) {
   res.render('cats', { bio: bio() });
-});
-
-router.get('/blog', function(req, res, next) {
-  res.render('blog', { bio: bio(), blog: blog() });
 });
 
 module.exports = router;
@@ -48,6 +48,9 @@ function bio() {
   return JSON.parse(fs.readFileSync('content/bio.json', 'utf8'));
 }
 
-function blog() {
-  return JSON.parse(fs.readFileSync('content/blog.json', 'utf8'));
+function portfolio() {
+  return {
+    'Blog': JSON.parse(fs.readFileSync('content/blog.json', 'utf8')),
+    'Open Source Contributions': JSON.parse(fs.readFileSync('content/open_source.json', 'utf8'))
+  }
 }
